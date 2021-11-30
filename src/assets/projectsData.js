80b9bad819c81931f9980836b19e75e173a6e3b5
Vibhu Sharma <vibhusharma@Vibhus-MacBook-Air.local>
2021-11-30 05:54:54 +0530

    image issue solved

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -1,9 +1,13 @@
+import proj1 from './../assets/proj1.PNG';
+import proj2 from './../assets/proj2.PNG';
+import proj5 from './../assets/proj5.PNG';
+
 export const projectsData = [
     {
         projectName: 'Name It!',
         projectDescription:
             'Do you find it difficult to name your projects or ventures? Fear not! Check out this cool name suggestion application which will help you find the best name for your next project, venture, or startup. Keep building!',
-        imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj1.PNG',
+        imageUrl: proj1,
         gitUrl:'https://github.com/vibhu8447/Name_It',
         projectUrl: 'https://vibhu8447.github.io/Name_It/',
     },
@@ -11,7 +15,7 @@ export const projectsData = [
         projectName: 'FlashType',
         projectDescription:
             'Having a good typing speed will always help you as a developer! Here, I present you the typing speed test application, where you can check your typing speed (in wpm) and share the results with your friends too.',
-        imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj2.PNG',
+            imageUrl: proj2,
         gitUrl:'https://github.com/vibhu8447/typing-test',
         projectUrl: 'https://vibhu8447.github.io/typing-test/',
 
@@ -21,8 +25,8 @@ export const projectsData = [
         projectName: 'Portfolio Website',
         projectDescription:
             'As a developer/designer, you should definitely have your own portfolio website. There are endless benefits. But, a lot of people feel that it would be tough to make one. So here I bring you a tutorial to make your own website.',
-        imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj5.PNG',
-        gitUrl: 'https://github.com/vibhu8447/Portfolio',
+        imageUrl: proj5,
+        gitUrl: 'https://github.com/vibhu8447/porfolio',
         projectUrl: '#',
     },
 ];
